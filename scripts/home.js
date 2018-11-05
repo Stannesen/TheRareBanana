2683b30cdd020054168ecb114f53cbaaf53e0fe8
Arne Johannesen <arne.johannesen00@gmail.com>
2018-11-05 21:11:37 +0100

    adding image to home

---
+++ scripts/home.js
@@ -0,0 +1,11 @@
+var rightButton = document.getElementById("right-button");
+var leftButton = document.getElementById("left-button");
+
+
+function rightClick(){
+    alert("Left");
+}
+
+function leftClick(){
+    alert("right");
+}
