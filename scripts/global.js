5c16d950bc3f64b2954532d10c539c4919605903
Arne Johannesen <arne.johannesen00@gmail.com>
2018-11-10 17:34:48 +0100

    optimizing for mobile

--- scripts/global.js
+++ scripts/global.js
@@ -1,3 +1,14 @@
 function logoClick(){
     window.location.href = "../index.html"
 }
+
+function mobileMenu(){
+    var mobileMenuHoler = document.getElementById("mobile-nav");
+    var currentDisplay = mobileMenuHoler.style.display;
+
+    if(currentDisplay == "block"){
+        mobileMenuHoler.style.display = "none";
+    }else{
+        mobileMenuHoler.style.display = "block";
+    }
+}
