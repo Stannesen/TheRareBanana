e7a70227244e216d2be0292c5c9454faf6d5292a
Arne Johannesen <arne.johannesen00@gmail.com>
2018-12-03 08:50:11 +0100

    Adding Time Converter

---
+++ scripts/time-converter.js
@@ -0,0 +1,39 @@
+var input = document.getElementById("time-input");
+
+var resultObj = document.getElementsByClassName("result");
+var convertFromItem = document.getElementsByClassName("convert-from__item");
+
+var timeList = [];
+var units = ["Millisecond", "Secund", "Minute", "Hour", "Day", "week"];
+var unitsValue = [0.001, 1, 60, 3600, 86400, 604800];
+
+var result = [""];
+var resultName = []
+
+input.oninput = function(){
+    var convertFromIndexObj = document.getElementById("convert-from__select").selectedIndex;
+    var convertFromValueObj = document.getElementById("convert-from__select").options;
+
+    var index = convertFromValueObj[convertFromIndexObj].index;
+    var value = input.value;
+
+    for(var i = 0; i < resultObj.length; i++){
+        result[i] = calculate(index, i, value);   
+    }
+
+    for(var i = 0; i < resultObj.length; i++){
+        resultObj[i].innerHTML = result[i];
+    }
+};
+
+
+function calculate(from, to, value){
+    var resultHelp = 0;
+    var calcHelp = 0;
+
+    calcHelp = value * unitsValue[from];
+    calcHelp = calcHelp / unitsValue[to];
+    resultHelp = units[to] + ": " + calcHelp;
+
+    return resultHelp;
+}
