29c2f34f7757a97bd1f4ea7e8dc8c643af026373
Arne Johannesen <arne.johannesen@gmail.com>
2019-02-15 09:23:06 +0100

    Adding new component to angular

--- react-app/src/components/header.js
+++ react-app/src/components/header.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
+import "../css/header.scss";
 
 
 class header extends Component {
     render() {
         return (
-            <div className="header">
-                <img className="logo__img" src={'/img/logoTest4.png'} alt="Can't find img"></img>
-                <div className="nav">
-                    <ul className="nav__ul">
-                        <li className="nav__li">
-                            <a className="nav__link" href="/">Home</a>
-                        </li>
-                    </ul>
+            <div>
+                <div className="header">
+                    <img className="logo__img" src={'/img/logoTest4.png'} alt="Can't find img"></img>
+                </div>
+
+                <div>
+                    Hey
                 </div>
             </div>
         );
